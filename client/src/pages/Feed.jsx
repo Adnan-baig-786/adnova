af36f7ebca802d0d699027956972c0368c51e36d
Adnan Baig <adnanbaig092@gmail.com>
2026-08-26 18:18:08 +0530

    spons

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -60,8 +60,8 @@ const Feed = () => {
         <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
           <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
           <img src={assets.sponsored_img} className='w-75 h-50 rounded-md object-cover' alt="" />
-          <p className='text-slate-600 font-medium'>Email marketing</p>
-          <p className='text-slate-400'>Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
+          <p className='text-slate-600 font-medium'>CareerX</p>
+          <p className='text-slate-400'>Build a better career with powerful AI tools for resumes, interviews, skills, and opportunities.</p>
         </div>
         <RecentMessages/>
       </div>
